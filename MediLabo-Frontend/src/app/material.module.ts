cd97c12ed8ba7768b08895324d719374ab1c3bf3
artificialideas <amateuc@gmail.com>
2023-12-04 13:38:24 +0100

    WIP: form datepicker + radio

--- MediLabo-Frontend/src/app/material.module.ts
+++ MediLabo-Frontend/src/app/material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 
@@ -10,7 +15,12 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule
   ],
   exports: [
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule, 
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatCardModule, 
     MatListModule, 
   ],
