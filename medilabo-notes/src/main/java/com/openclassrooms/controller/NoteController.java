f8dd547ac46069b895f399fe57d2e78210206223
artificialideas <amateuc@gmail.com>
2023-12-20 10:26:11 +0100

    Done: removing security. ToDo: validate early_onset user.

--- medilabo-notes/src/main/java/com/openclassrooms/controller/NoteController.java
+++ medilabo-notes/src/main/java/com/openclassrooms/controller/NoteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/notes")
+@RequestMapping("/notes")
 public class NoteController {
     @Autowired
     private NoteService noteService;
--- medilabo-patients/src/main/java/com/openclassrooms/controller/PatientController.java
+++ medilabo-patients/src/main/java/com/openclassrooms/controller/PatientController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/patients")
+@RequestMapping("/patients")
 public class PatientController {
     @Autowired
     private PatientService patientService;
