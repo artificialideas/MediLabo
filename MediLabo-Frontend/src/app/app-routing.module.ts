760b2589391c5ebb391ecee0979787b2e62155ca
artificialideas <amateuc@gmail.com>
2023-12-28 18:11:18 +0100

    fail 4

--- MediLabo-Frontend/src/app/app-routing.module.ts
+++ MediLabo-Frontend/src/app/app-routing.module.ts
@@ -9,13 +9,12 @@ import { Page404Component } from './components/patient/page-404/page-404.compone
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'login',
+  /* { path: '',
     component: LoginComponent,
     title: 'MediLabo',
-  },
-  { path: '', pathMatch: 'full', redirectTo: 'patients'},
+  }, */
   {
-    path: 'patients',
+    path: '',
     component: PatientComponent,
     title: 'Patients list'
   },
