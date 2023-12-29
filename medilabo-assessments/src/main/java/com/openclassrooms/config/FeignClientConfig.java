f9272d401122b441e042354bda0b6a29688f29a0
artificialideas <amateuc@gmail.com>
2023-12-29 16:08:14 +0100

    WIP: assessments with security doesn't work. ToDo: Front login validation.

---
+++ medilabo-assessments/src/main/java/com/openclassrooms/config/FeignClientConfig.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.config;
+
+import feign.RequestInterceptor;
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new BasicAuthRequestInterceptor("admin", "medilabo");
+    }
+}
--- medilabo-assessments/src/main/java/com/openclassrooms/config/FeignConfig.java
+++ medilabo-assessments/src/main/java/com/openclassrooms/config/FeignConfig.java
@@ -1,25 +1,25 @@
-package com.openclassrooms.config;
-
-import feign.RequestInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
-@Configuration
-public class FeignConfig {
-    @Bean
-    public RequestInterceptor requestInterceptor() {
-        return requestTemplate -> {
-            // Retrieve security context from the original request
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-            // Pass authentication details to the Feign client request
-            if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-                requestTemplate.header("Authorization", "Bearer " + userDetails.getUsername());
-            }
-        };
-    }
-}
+//package com.openclassrooms.config;
+//
+//import feign.RequestInterceptor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//@Configuration
+//public class FeignConfig {
+//    @Bean
+//    public RequestInterceptor requestInterceptor() {
+//        return requestTemplate -> {
+//            // Retrieve security context from the original request
+//            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//            // Pass authentication details to the Feign client request
+//            if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+//                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//                requestTemplate.header("Authorization", "Bearer " + userDetails.getUsername());
+//            }
+//        };
+//    }
+//}
