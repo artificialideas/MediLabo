fb6aed01b0b397f1bdc810266769cb45a6bb5929
artificialideas <amateuc@gmail.com>
2023-12-29 15:46:11 +0100

    Done: Security auth work for all services' endpoints (with and without gateway) except assessments. Front not validated.

--- medilabo-gateway/src/main/java/com/openclassrooms/config/SecurityConfig.java
+++ medilabo-gateway/src/main/java/com/openclassrooms/config/SecurityConfig.java
@@ -1,30 +1,30 @@
-package com.openclassrooms.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("admin")
-//                .password("medilabo") // For testing purpose only
-//                .roles("ADMIN")
-//                .build();
+//package com.openclassrooms.config;
 //
-//        return new InMemoryUserDetailsManager(user);
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+////    @Bean
+////    @Override
+////    public UserDetailsService userDetailsService() {
+////        UserDetails user = User.withUsername("admin")
+////                .password("medilabo") // For testing purpose only
+////                .roles("ADMIN")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(user);
+////    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .httpBasic()
+//            .and()
+//            .authorizeRequests()
+//            .antMatchers("/").permitAll()
+//            .anyRequest().authenticated();
 //    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .httpBasic()
-            .and()
-            .authorizeRequests()
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated();
-    }
-}
+//}
--- medilabo-notes/src/main/java/com/openclassrooms/config/SecurityConfig.java
+++ medilabo-notes/src/main/java/com/openclassrooms/config/SecurityConfig.java
@@ -1,18 +1,21 @@
 package com.openclassrooms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/notes/**").hasRole("ADMIN")
-            .and().httpBasic()
-            .and().csrf().disable();
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
